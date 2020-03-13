3c5d00c704412a2d012a380a5c94bcd1f63b6f95
nojaja <free.riccia@gmail.com>
2020-03-13 20:11:04 +0900

    css/index.css追加

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 require('webpack-jquery-ui');
 var test = require('webpack-jquery-ui/css');  //ommit, if you don't want to load basic css theme
+require('../css/index.css');
 
 $( "#projects" ).accordion();
